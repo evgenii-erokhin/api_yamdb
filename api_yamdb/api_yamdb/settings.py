e8b479bd765a82e8f5416898b2846f29b12393ce
Dmitry Borisenko <borisenko.production@gmail.com>
2023-04-13 00:10:11 +0300

    Added Users app and created User model.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.apps.UsersConfig',
 ]
 
 ROOT_URLCONF = 'api_yamdb.urls'
