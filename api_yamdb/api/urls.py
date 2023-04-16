2f98e4d8b9706e0b44b97af14c34e1a56cfa1aa3
Evgenii Erokhin <evgeniierokhin@proton.me>
2023-04-16 22:20:08 +0300

    Added viewsets and serializers

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,20 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
+from api.views import (CategoryViewSet, GenreViewSet, TitleViewSet,
+                       CommentsViewSet, ReviewViewSet)
 
-router = routers.DefaultRouter()
-router.register('v1/categories', CategoryViewSet)
-router.register('v1/genres', GenreViewSet)
-router.register('v1/titles', TitleViewSet)
+router_v1 = routers.DefaultRouter()
+router_v1.register('v1/categories', CategoryViewSet)
+router_v1.register('v1/genres', GenreViewSet)
+router_v1.register('v1/titles', TitleViewSet)
+router_v1.register(
+    r'v1/titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentsViewSet, basename='comments'
+)
+router_v1.register(r'v1/titles/(?P<title_id>\d+)reviews',
+                   ReviewViewSet, basename='reviews')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
 ]
