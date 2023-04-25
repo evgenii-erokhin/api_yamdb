b1a6cd04ed056c8da61e7892bc6d44c5160e03e4
Дмитрий Фофанов <dmitriefofanov@yandex.ru>
2023-04-25 18:27:57 +0300

    Fix

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -11,27 +11,29 @@ class IsAdmin(IsAdminUser):
         user = User.objects.get(id=request.user.id)
         return bool(request.user and user.role == 'admin')
 
+    def has_object_permission(self, request, view, obj):
+        if request.user.id is None:
+            return False
+        user = User.objects.get(id=request.user.id)
+        return bool(request.user and user.role == 'admin')
+
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-    
+        return bool(request.method in SAFE_METHODS)
+
     def has_object_permission(self, request, view, obj):
         return bool(request.method in SAFE_METHODS)
 
 
-class IsAdminOrAuthor(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.id is None:
-            return False
-        user = User.objects.get(id=request.user.id)
-        return bool(user.role == 'admin' or user == obj.author)
-
-
 class IsAuthorOrModer(BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user.id is not None)
+
     def has_object_permission(self, request, view, obj):
         if request.user.id is None:
             return False
         user = User.objects.get(id=request.user.id)
+        print(user.role, user.id, obj.author.id)
         return bool(user.role == 'moderator' or user == obj.author)
  
