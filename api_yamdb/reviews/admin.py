b36aa90a51badf832397c3c331e752565e3d6815
Dmitry-Fofanov <119697624+Dmitry-Fofanov@users.noreply.github.com>
2023-05-02 12:01:36 +0300

    Revert "Develop into master"

--- api_yamdb/reviews/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Category, Comment, Genre, Review, Title
-
-admin.site.register(Title)
-admin.site.register(Category)
-admin.site.register(Review)
-admin.site.register(Comment)
-admin.site.register(Genre)
